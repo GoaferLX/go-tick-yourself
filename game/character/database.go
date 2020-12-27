fe9f80dcbdb4f426536f1fdb1c96e6c3ee923519
Matthew Chipperfield <58623934+GoaferLX@users.noreply.github.com>
2020-12-27 08:45:54 +0000

    removed superfluous call to err.Error() in getCharacter

--- game/character/database.go
+++ game/character/database.go
@@ -11,10 +11,6 @@ type character struct {
 func getCharacter(db *sql.DB, id uint64) (character, error) {
 	row := db.QueryRow("SELECT * FROM characters WHERE id = $1", id)
 
-	if err := row.Err(); err != nil {
-		return character{}, err
-	}
-
 	var c character
 	if err := row.Scan(&c.id, &c.balance, &c.reputation); err != nil {
 		return character{}, err
